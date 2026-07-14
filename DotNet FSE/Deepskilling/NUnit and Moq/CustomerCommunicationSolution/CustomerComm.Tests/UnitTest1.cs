3bc3480390f51cad839b58259a8a99d6d89e0f13
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-14 14:52:54 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/NUnit and Moq/CustomerCommunicationSolution/CustomerComm.Tests/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using CustomerCommLib;
+using Moq;
+using NUnit.Framework;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        private Mock<IMailSender> mockMailSender;
+
+        // Notice CustomerCommLib.CustomerComm
+        private CustomerCommLib.CustomerComm customerComm;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            mockMailSender = new Mock<IMailSender>();
+
+            mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            customerComm = new CustomerCommLib.CustomerComm(mockMailSender.Object);
+        }
+
+        [Test]
+        public void Test_SendMailToCustomer_ReturnsTrue()
+        {
+            bool result = customerComm.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
