9cc921579d1e5001aee70d7fbcdb9a87f5e16fa5
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-14 15:07:51 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Entity Framework Core/Lab 2: Setting Up the Database Context for a Retail Store/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace RetailInventory.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; } = string.Empty;
+
+        public List<Product> Products { get; set; } = new();
+    }
+}
