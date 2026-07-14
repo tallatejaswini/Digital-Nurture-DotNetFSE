402cd0f602768ff0b2cc176b063e5db590a3790f
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-14 14:54:33 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/NUnit and Moq/CustomerCommunicationSolution/CustomerCommLib/MailSender.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace CustomerCommLib
+{
+    public class MailSender : IMailSender
+    {
+        public bool SendMail(string toAddress, string message)
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("your_email@gmail.com");
+            mail.To.Add(toAddress);
+            mail.Subject = "Test Mail";
+            mail.Body = message;
+
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("username", "password");
+            smtp.EnableSsl = true;
+
+            //smtp.Send(mail);
+
+            return true;
+        }
+    }
+}
