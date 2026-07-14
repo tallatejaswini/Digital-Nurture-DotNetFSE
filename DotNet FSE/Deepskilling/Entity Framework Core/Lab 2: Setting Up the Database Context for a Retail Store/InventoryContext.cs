acf8298ca5cca5e694adf20e322634fe3f91fa8e
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-14 15:09:03 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Entity Framework Core/Lab 2: Setting Up the Database Context for a Retail Store/InventoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory.Data
+{
+    public class InventoryContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=RetailInventoryDB;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
