23b48619ad96bfc615672527661431e9e70e4acc
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-22 11:46:25 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Microservices/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroserviceAuth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("dashboard")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAdminDashboard()
+        {
+            return Ok("Welcome to the admin dashboard.");
+        }
+    }
+}
---
+++ DotNet FSE/Deepskilling/Microservices/AuthController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MicroserviceAuth.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MicroserviceAuth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginModel model)
+        {
+            if (model.Username == "admin" &&
+                model.Password == "admin123")
+            {
+                var token = GenerateJwtToken(model.Username);
+
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized();
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim(ClaimTypes.Role,"Admin")
+            };
+
+            var key =
+new SymmetricSecurityKey(
+Encoding.UTF8.GetBytes(
+"ThisIsASecretKeyForJwtToken1234567890"));
+
+            var creds =
+                new SigningCredentials(
+                    key,
+                    SecurityAlgorithms.HmacSha256);
+
+            var token =
+                new JwtSecurityToken(
+                    issuer: "MyAuthServer",
+                    audience: "MyApiUsers",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler()
+                .WriteToken(token);
+        }
+    }
+}
