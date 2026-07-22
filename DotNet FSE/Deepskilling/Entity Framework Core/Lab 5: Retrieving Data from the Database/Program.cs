5fa138e3b9f64a756ef3dfd23555351013e56a4f
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-22 11:34:57 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Entity Framework Core/Lab 5: Retrieving Data from the Database/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new InventoryContext();
+
+Console.WriteLine("----- ALL PRODUCTS -----");
+
+var products = await context.Products.ToListAsync();
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.ProductName} - ₹{p.Price}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("----- FIND BY ID -----");
+
+var product = await context.Products.FindAsync(1);
+
+if (product != null)
+{
+    Console.WriteLine($"Found: {product.ProductName}");
+}
+else
+{
+    Console.WriteLine("Product not found.");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("----- FIRST PRODUCT PRICE > 50000 -----");
+
+var expensive = await context.Products
+    .FirstOrDefaultAsync(p => p.Price > 50000);
+
+if (expensive != null)
+{
+    Console.WriteLine($"Expensive Product: {expensive.ProductName}");
+}
+else
+{
+    Console.WriteLine("No expensive product found.");
+}
