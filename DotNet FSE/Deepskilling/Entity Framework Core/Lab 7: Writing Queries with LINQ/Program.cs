29414ea17fa56314c00e8ab9137ce750fe6bfc42
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-22 11:41:50 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Entity Framework Core/Lab 7: Writing Queries with LINQ/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new InventoryContext();
+
+Console.WriteLine("----- PRODUCTS PRICE > 1000 -----");
+
+var filtered = await context.Products
+    .Where(p => p.Price > 1000)
+    .OrderByDescending(p => p.Price)
+    .ToListAsync();
+
+foreach (var p in filtered)
+{
+    Console.WriteLine($"{p.ProductName} - ₹{p.Price}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("----- PRODUCT DTO -----");
+
+var productDTOs = await context.Products
+    .Select(p => new
+    {
+        p.ProductName,
+        p.Price
+    })
+    .ToListAsync();
+
+foreach (var p in productDTOs)
+{
+    Console.WriteLine($"{p.ProductName} - ₹{p.Price}");
+}
