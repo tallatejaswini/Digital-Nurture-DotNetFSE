47612eb84171e5eb55a8b16dc25818b1e5a37374
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-22 11:36:14 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/Entity Framework Core/Lab 6: Updating and Deleting Records/Program (1).cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new InventoryContext();
+
+// Update Product
+var product = await context.Products
+    .FirstOrDefaultAsync(p => p.ProductName == "Laptop");
+
+if (product != null)
+{
+    product.Price = 70000;
+
+    await context.SaveChangesAsync();
+
+    Console.WriteLine("Product price updated.");
+}
+else
+{
+    Console.WriteLine("Laptop not found.");
+}
+
+// Delete Product
+var toDelete = await context.Products
+    .FirstOrDefaultAsync(p => p.ProductName == "Rice Bag");
+
+if (toDelete != null)
+{
+    context.Products.Remove(toDelete);
+
+    await context.SaveChangesAsync();
+
+    Console.WriteLine("Product deleted.");
+}
+else
+{
+    Console.WriteLine("Rice Bag not found.");
+}
