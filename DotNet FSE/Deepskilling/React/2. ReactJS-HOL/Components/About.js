0ef62a7144408ced750fb3a5135cd73a33171add
tallatejaswini <tallatejaswini20@gmail.com>
2026-07-22 12:04:01 +0530

    Add files via upload

---
+++ DotNet FSE/Deepskilling/React/2. ReactJS-HOL/Components/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+    return (
+        <h1>Welcome to the About page of Student Management Portal</h1>
+    );
+}
+
+export default About;
---
+++ DotNet FSE/Deepskilling/React/2. ReactJS-HOL/Components/Contact.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <h1>Welcome to the Contact page of Student Management Portal</h1>
+    );
+}
+
+export default Contact;
